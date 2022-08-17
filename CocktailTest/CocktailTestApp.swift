e2a1c14b71576e4af8867f7d7ec4f4832cefade0
Erwin Marysiok <erwinos753@gmail.com>
2022-08-17 13:06:38 +0200

    Initial Commit

---
+++ CocktailTest/CocktailTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  CocktailTestApp.swift
+//  CocktailTest
+//
+//  Created by Erwin Marysiok on 17/08/2022.
+//
+
+import SwiftUI
+
+@main
+struct CocktailTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
